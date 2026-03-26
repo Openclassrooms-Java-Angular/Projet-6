4c232c92cc205007b4ab647dcd8aca1bf8dcad92
Jocelyn Flament <jocelyn.flament@gmail.com>
2026-03-26 04:41:18 +0100

    chore(front): update Angular config and standalone bootstrap setup

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
+import { AuthInterceptor } from './services/auth.interceptor';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopicComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+imports: [
+  BrowserModule,
+  BrowserAnimationsModule,
+  MatButtonModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  MatFormFieldModule,
+  MatInputModule,
+],
+declarations: [
+  AppComponent,
+  RegisterComponent,
+  HomeComponent
+],
+providers: [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+],
+bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,12 +1,45 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/pages/home/home.component';
+import { LoginComponent } from './app/pages/login/login.component';
+import { RegisterComponent } from './app/pages/register/register.component';
 import { environment } from './environments/environment';
+
+// Angular Material modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {provideHttpClient} from "@angular/common/http";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '**', redirectTo: '' }
+    ]),
+
+    // Animations (une seule méthode)
+    provideAnimations(),
+
+    provideHttpClient(),
+
+    // Material modules
+    importProvidersFrom([
+      MatButtonModule,
+      MatFormFieldModule,
+      MatInputModule
+      // + autres modules Material si besoin
+    ])
+  ]
+})
   .catch(err => console.error(err));
