f4262a02b3a4ec6cce116829c8779e8bf82b609b
Jocelyn Flament <jocelyn.flament@gmail.com>
2026-04-13 05:51:46 +0200

    feat(back): add OpenAPI/Swagger configuration

---
+++ back/src/main/java/com/openclassrooms/mddapi/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.mddapi.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Rental API")
+                        .version("1.0")
+                        .description("API Monde de Dév")
+                        .contact(new Contact()
+                                .name("Jocelyn")
+                                .email("jocelyn@mdd"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                // appliquer la sécurité globalement (toutes les opérations exigent le bearer par défaut)
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+    }
+}
