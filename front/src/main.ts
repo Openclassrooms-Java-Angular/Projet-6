84806d8f71d118928913bbbf56fabde9562c2a58
Jocelyn Flament <jocelyn.flament@gmail.com>
2026-04-15 11:15:22 +0200

    feat(front): add new routes

--- front/src/main.ts
+++ front/src/main.ts
@@ -7,13 +7,18 @@ import { AppComponent } from './app/app.component';
 import { HomeComponent } from './app/pages/home/home.component';
 import { LoginComponent } from './app/pages/login/login.component';
 import { RegisterComponent } from './app/pages/register/register.component';
+import { FeedComponent } from './app/pages/feed/feed.component';
 import { environment } from './environments/environment';
 
 // Angular Material modules
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
+import { TopicsComponent } from './app/pages/topics/topics.component';
+import { PostDetailComponent } from './app/pages/post-detail/post-detail.component';
+import { CreatePostComponent } from './app/pages/create-post/create-post.component';
+import { ProfileComponent } from './app/pages/profile/profile.component';
 
 if (environment.production) {
   enableProdMode();
@@ -25,6 +30,11 @@ bootstrapApplication(AppComponent, {
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
+      { path: 'feed', component: FeedComponent },
+      { path: 'posts/:id', component: PostDetailComponent },
+      { path: 'topics', component: TopicsComponent },
+      { path: 'create-post', component: CreatePostComponent },
+      { path: 'profile', component: ProfileComponent },
       { path: '**', redirectTo: '' }
     ]),
 
